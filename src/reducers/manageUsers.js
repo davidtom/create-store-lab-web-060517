725e8eb1d3a6514fd425bb53d65f74360a327d12
davidtom <davidjtomczyk@gmail.com>
2017-08-27 15:49:25 -0400

    complete lab

--- src/reducers/manageUsers.js
+++ src/reducers/manageUsers.js
@@ -1,2 +1,9 @@
-export default function manageUsers(state, action){
+export default function manageUsers(state={users:[]}, action){
+  switch(action.type){
+    case "ADD_USER":
+    console.log(action.action)
+      return Object.assign({}, state, {users: state.users.concat({username: action.user.username, hometown: action.user.hometown})})
+    default:
+      return state
+  }
 }
